52a9a89766386ae30708cebe653ca5cf7d03884f
Prakash Sakari <sakariprakash@gmail.com>
2022-06-04 11:10:46 +0530

    added products

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+export const Navbar = () => {
+    return (
+        <header class="heading d-flex grow-shrink-basis align-center">
+            <div class="heading-title-icon d-flex grow-shrink-basis align-center">
+                {/* <img class="icon mr-1" src="/assets/image.png" alt="lightbul" /> */}
+                <h1 class="heading-title">
+                    <a class="link" href="/">Apparel Kart</a>
+                </h1>
+            </div>
+            <nav class="navigation">
+                <ul class="list-non-bullet">
+                    <li class="list-item-inline">
+                        <Link to="/" class="link cursor">Home</Link>
+                    </li>
+                </ul>
+            </nav>
+</header>
+    )
+}
---
+++ src/db/data.js
@@ -0,0 +1,104 @@
+import { v4 as uuid } from "uuid";
+
+export const data = [
+    {
+        _id: uuid(),
+        name: "Men Checked Shirt",
+        img: "https://rukminim1.flixcart.com/image/880/1056/kwtkxow0/shirt/c/u/n/xl-brd-white-p-v-creations-original-imag9f3yxz7d89t4.jpeg?q=50",
+        alt: "mens shirt",
+        brand: "Roadster",
+        price: 799,
+        size: "L",
+        idealFor: "M",
+        rating: 4.2
+    },
+    {
+        _id: uuid(),
+        name: "Women Crop Top",
+        img: "https://rukminim1.flixcart.com/image/880/1056/krf91u80/top/g/n/v/xs-lbt-13-maroon-london-belly-original-imag57qgzcm3hsmv.jpeg?q=50",
+        alt: "womens top",
+        brand: "Forever 21",
+        price: 1599,
+        size: "M",
+        idealFor: "W",
+        rating: 4.5
+    },
+    {
+        _id: uuid(),
+        name: "Women Solid Shirt",
+        img: "https://rukminim1.flixcart.com/image/880/1056/kf2v3ww0/top/z/f/t/l-044-yukon01-original-imafvhkejzfhqaqz.jpeg?q=50",
+        alt: "Women shirt",
+        brand: "Zara",
+        price: 2999,
+        size: "L",
+        idealFor: "W",
+        rating: 3.9
+    },
+    {
+        _id: uuid(),
+        name: "Men Solid Shirt",
+        brand: "Peter England",
+        img: "https://rukminim1.flixcart.com/image/580/696/kwzap3k0/shirt/r/q/j/l-lstr-acqua-p-v-creations-original-imag9jggj6naa4cu.jpeg?q=50",
+        alt: "mens shirt",
+        price: 1899,
+        size: "XL",
+        idealFor: "M",
+        rating: 3.5
+    },
+    {
+        _id: uuid(),
+        name: "Men Solid T-Shirt",
+        brand: "Roadster",
+        img: "https://rukminim1.flixcart.com/image/580/696/kz8qsnk0/t-shirt/g/z/n/m-db1024-42-3bros-original-imagbawh38j3ghzr.jpeg?q=50",
+        alt: "mens t-shirt",
+        price: 399,
+        size: "M",
+        idealFor: "M",
+        rating: 3.2
+    },
+    {
+        _id: uuid(),
+        name: "Women Printed Top",
+        img: "https://rukminim1.flixcart.com/image/580/696/kzhbfrk0/top/v/b/6/m-fancy-top-shree-villa-original-imagbhgpqkj9snta.jpeg?q=50",
+        alt: "Women Printed Top",
+        brand: "Zara",
+        price: 1799,
+        size: "S",
+        idealFor: "W",
+        rating: 3.2
+    },
+    {
+        _id: uuid(),
+        name: "Men Regular V Neck T-Shirt",
+        img: "https://rukminim1.flixcart.com/image/580/696/k6gsk280/t-shirt/e/z/h/xl-pp-374-385-379-revesto-original-imafkxwnetcxt4xr.jpeg?q=50",
+        alt: "mens t-shirt",
+        brand: "DNMX",
+        price: 499,
+        size: "XXL",
+        idealFor: "M",
+        rating: 2.2
+    },
+    {
+        _id: uuid(),
+        name: "Women Regular Top",
+        img: "https://rukminim1.flixcart.com/image/580/696/knni7ww0/top/o/m/o/s-dmp303-dmp-fashion-original-imag2a5qkjgjf22k.jpeg?q=50",
+        alt: "Women Regular Top",
+        brand: "Forever 21",
+        price: 1799,
+        size: "XXL",
+        idealFor: "W",
+        rating: 2.2
+    },
+    {
+        _id: uuid(),
+        name: "Women Solid T-Shirt",
+        img: "https://rukminim1.flixcart.com/image/580/696/jzn0tjk0/t-shirt/t/y/g/s-fw-014-mrrd-s-first-wave-original-imaf2nxuqatamwhm.jpeg?q=50",
+        alt: "Women t-shirt",
+        brand: "Forever 21",
+        price: 799,
+        size: "S",
+        idealFor: "W",
+        rating: 2.2
+    },
+
+]
